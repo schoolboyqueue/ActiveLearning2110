ec7e0f4f7eeeb43f0b4b0fd386faaddd55d8e96f
ODell Mizrahi <ODell Mizrahi>
2016-09-18 18:03:34 -0400

    adding controller set up for app api

--- app_api/routes/userRouter.js
+++ app_api/routes/userRouter.js
@@ -1,12 +1,11 @@
 "use strict"
 
 var express = require('express');
+var userController = require('./../controllers/userController')
 
 var useRouter = express.Router();
 
 useRouter.route('')
-    .get(function (req, res) {
-        res.send('list of user');
-    });
+    .get(userController);
 
 module.exports = useRouter;
