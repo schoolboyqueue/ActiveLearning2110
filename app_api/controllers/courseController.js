79da5248a82aa9532166c4daa2732ed86229be42
ODell Mizrahi <odellm81@live.com>
2016-11-20 12:24:24 -0500

    basic course model, router, controller. Also adding auth controller

--- app_api/controllers/courseController.js
+++ app_api/controllers/courseController.js
@@ -1,63 +1,109 @@
 /* jshint node: true */
 
 //************************************************************
-//  userController.js                                       //
+//  courseController.js                                     //
 //  Active Learning 2110                                    //
 //                                                          //
-//  Created by Odell Mizrahi on 9/18/16.                    //
+//  Created by Odell Mizrahi on 11/19/16.                   //
 //  Copyright © 2016 Odell Mizrahi. All rights reserved.    //
 //                                                          //
 //  Date        Name        Description                     //
 //  -------     ---------   --------------                  //
-//  18Sep16     O. Mizrahi  Initial Design                  //
+//  19Nov16     O. Mizrahi  Initial Design                  //
 //                                                          //
 //************************************************************
 "use strict";
-//var User    = require('./../models/userModel');
-//var Course  = require('./../models/courseModel');
-//var rand = require("random-key");
+
+var Course  = require('./../models/courseModel');
+var rand = require("random-key");
+
+var roles =
+{
+    ADMIN: 'admin',
+    INSTRUCTOR: 'instructor',
+    STUDENT: 'student',
+};
 
 var createCourse  = function (req, res)
 {
+    var newCourse = null;
 
-    console.log(req.session.user.role);
-    res.status(201).json(
+    var course_instructor =
+    {
+        instructor_id : req.user.id.toString(),
+        username:       req.user.username,
+    }
+
+    newCourse = new Course(
+    {
+        title:          req.body.title,
+        instructor:     course_instructor,
+        access_key:     rand.generate()
+    });
+
+    newCourse.save(function(err, savedCourse)
+    {
+        if (err)
         {
-            success: true,
-            message: 'Course Creation Successsful'
+            return res.status(500).json(
+                {
+                    success: false,
+                    message: "Internal Error"
+                }
+            );
         }
-    );
+        res.status(201).json(
+            {
+                success: true,
+                message: 'Course Creation Successsful',
+                course :  savedCourse
+            }
+        );
+    });
 };
 
-var requireInstuct = function (req, res, next)
+var getAllCourses  = function (req, res)
 {
-  if (!req.user)
-  {
-      return res.status(401).json(
-          {
-              success: false,
-              message: 'No Session Active'
-          }
-      );
-  }
-  if (req.user.role != "instructor")
-  {
-      return res.status(401).json(
-          {
-              success: false,
-              message: 'Admin Authorization Required'
-          }
-      );
-  }
-  else
-  {
-      next();
-  }
+    Course.find(function(err, courses)
+    {
+        if (err)
+        {
+            res.status(500);
+            res.send('Internal Error');
+        }
+        else
+        {
+            res.status(200);
+            res.send(courses);
+        }
+    });
+};
 
+var getInstructorCourses = function (req, res)
+{
+    Course.find({"instructor.instructor_id": req.params.USERID}, function(err, courses)
+    {
+        if (err || !courses)
+        {
+            return res.status(404).json(
+                {
+                    success: false,
+                    message: 'No Courses Not Found'
+                }
+            );
+        }
+        res.status(200).json(
+            {
+                success: true,
+                courses: courses
+            }
+        );
+    });
 };
 
 module.exports =
 {
-    createCourse      :    createCourse,
-    requireInstuct    :    requireInstuct
+    createCourse        :    createCourse,
+    getAllCourses       :    getAllCourses,
+    getInstructorCourses:    getInstructorCourses
 };
