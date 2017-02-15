4b67721003e9947e4394afa29c81b845d7eb6f39
ODell Mizrahi <odellm81@live.com>
2017-02-15 13:40:36 -0500

    updates to student complete register and instructor delete student

--- app_api_v2/routes/courseRouter.js
+++ app_api_v2/routes/courseRouter.js
@@ -116,12 +116,12 @@ courseRouter.route('/:COURSEID/students')
 /**
 INSTRUCTOR ADD STUDENTS
 
-POST  /api_v2/course/{course_id}/students/{user_id}/
+POST  /api_v2/course/{course_id}/sections/{section_id}/students/
 
 Authentication:   user token
 Authorization:    instructor
 
-Path Parameters:  none
+Path Parameters:  course_id, section_id String    required
 Query String:     none
 Request Body:     application/json    required
 {
@@ -131,7 +131,7 @@ Request Body:     application/json    required
 }
 **/
 courseRouter.route('/:COURSEID/sections/:SECTIONID/students/:USERID')
-   .put(tokenController.validateToken,
+   .post(tokenController.validateToken,
         tokenController.refreshToken,
         authorizeController.instructor,
         inputController.requireFirstname,
