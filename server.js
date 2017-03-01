e5a836afcf9e727cd2a9e3f5bd144cddc2a47814
Jeremy Carter <carter-int@hotmail.com>
2017-03-01 11:54:13 -0500

    Updated student dashboard and course view to include their section along with including it in the email subject line.

--- server.js
+++ server.js
@@ -37,13 +37,13 @@ mongoose.Promise = global.Promise;
 mongoose.connect(config.database);
 
 io.sockets
-  .on('connection', socketioJwt.authorize({
-    secret: config.jwt_secret,
-    timeout: 15000 // 15 seconds to send the authentication message
-  })).on('authenticated', function(socket) {
-    //this socket is authenticated, we are good to handle more events from it.
-    console.log('hello! ' + JSON.stringify(socket.decoded_token));
-  });
+    .on('connection', socketioJwt.authorize({
+        secret: config.jwt_secret,
+        timeout: 15000 // 15 seconds to send the authentication message
+    })).on('authenticated', function(socket) {
+        //this socket is authenticated, we are good to handle more events from it.
+        console.log('hello! ' + JSON.stringify(socket.decoded_token));
+    });
 
 app.use(express.static(path.join(__dirname, '/app_client')));
 
